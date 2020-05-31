9624fac03e1d55ba1478448fb54b63cd12b015d8
TanyaMio <32582073+TanyaMio@users.noreply.github.com>
2020-05-31 22:22:17 +0300

    Update MangSysApplication.java

--- src/main/java/com/training/demo/MangSysApplication.java
+++ src/main/java/com/training/demo/MangSysApplication.java
@@ -1,9 +1,12 @@
 package com.training.demo;
 
+import com.training.demo.controllers.util.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootApplication
+@ContextConfiguration(classes = { AppConfig.class })
 public class MangSysApplication {
 
     public static void main(String[] args) {
